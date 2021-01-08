853f0848751ea4c577b3ea2b6f1bf20295543fa0
eldiablo-1226 <shaxzod12262000@gmail.com>
2021-01-08 16:09:49 +0500

    1.3.4

--- src/BarCodeScanner/View/MainWindow.xaml.cs
+++ src/BarCodeScanner/View/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
-
+﻿using System;
+using System.Windows;
 using BarCodeScanner.ViewModel;
+using System.Drawing;
+using System.IO;
 
 namespace BarCodeScanner
 {
@@ -14,6 +16,30 @@ namespace BarCodeScanner
             InitializeComponent();
 
             DataContext = new MainViewModel();
+
+            NotifyIcon.Icon = new Icon(new FileStream("icons8_barcode.ico", FileMode.Open));
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+            if (WindowState == WindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                NotifyIcon.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void OpenClickTaskBar(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+            this.ShowInTaskbar = true;
+            NotifyIcon.Visibility = Visibility.Hidden;
+        }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- src/BarCodeScanner/ViewModel/MainViewModel.cs
+++ src/BarCodeScanner/ViewModel/MainViewModel.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Drawing;
+using System.IO;
 using CommonServiceLocator;
 
 using ReactiveUI;
